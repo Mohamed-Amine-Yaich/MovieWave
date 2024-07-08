241bb81690dd20aee1a21ae2515eb46ff3d154fa
Mohamed-Amine-Yaich <yaichamine04@gmail.com>
2024-07-08 18:23:40 +0100

    prepare detail screen components

--- app/detail.tsx
+++ app/detail.tsx
@@ -1,26 +1,53 @@
-import { fetchMovies } from '@/services/moviesService';
+import ErrorToast from '@/components/DetailScreen/ErrorToast';
+import MovieDetailBackButton from '@/components/DetailScreen/MovieDetailBackButton';
+import MovieDetailContent from '@/components/DetailScreen/MovieDetailContent';
+import MovieDetailPoster from '@/components/DetailScreen/MovieDetailsPoster';
+import { MovieDetails } from '@/interfaces/interfaces';
 import { useLocalSearchParams } from 'expo-router';
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, ScrollView } from 'react-native';
 
 interface DetailScreenProps { }
 
 const DetailScreen = (props: DetailScreenProps) => {
   const params = useLocalSearchParams();
   console.log(params)
+  const [movie, setMovie] = useState<MovieDetails>();
+  const [loading, setLoading] = useState(false);
+  const [apiError, setApiError] = useState<string>('');
+  const [error, setError] = useState<string>('gg');
+
+
+  function hideErrorToast(): void {
+    setError('')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>DetailScreen {params?.movieId} </Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.scrollViewContent}>
+
+      <MovieDetailBackButton />
+      <MovieDetailPoster loading={loading} movie={movie} />
+
+      {movie && <MovieDetailContent movie={movie} />}
+
+      {(error || apiError) && (
+        <ErrorToast
+          isVisible={!!(error || apiError)}
+          message={error ? "An error has occurred. Please try later." : apiError && 'Movie could not be loaded. Please try again later.'}
+          onClose={hideErrorToast}
+        />
+      )}
+    </ScrollView>
   );
 };
 
 export default DetailScreen;
 
 const styles = StyleSheet.create({
-  container: {
+  scrollViewContent: {
     flex: 1,
-    position: 'relative',
+    backgroundColor: '#f8f8f8',
   },
 
 })
